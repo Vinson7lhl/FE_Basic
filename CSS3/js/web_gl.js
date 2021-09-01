9dbfcf39e26f9a36b793db2167c0e149931d8269
Seven Li-后会有七 <vinson7lhl@126.com>
2021-09-01 15:02:03 +0800

    李宏磊/style-feat/增加webgl测试

--- CSS3/js/web_gl.js
+++ CSS3/js/web_gl.js
@@ -1,3 +1,7 @@
 $(function() {
     console.log('WebGL准备完毕')
+    let can_container = $('#web_gl_container').get(0)
+    let ctx = can_container.getContext('2d')
+    ctx.fillStyle = 'rgba(0, 0, 255, 1)'
+    ctx.fillRect(10, 10, 200, 200)
 })
