934649c0aa379b491d13c80bd0a07eb76bbb9412
Seven Li-后会有七 <vinson7lhl@126.com>
2021-09-02 16:08:26 +0800

    李宏磊/style/grid布局

--- CSS3/js/web_gl.js
+++ CSS3/js/web_gl.js
@@ -1,7 +1,14 @@
 $(function() {
     console.log('WebGL准备完毕')
+    // 获取canvas-dom容器
     let can_container = $('#web_gl_container').get(0)
+    // 获取绘制的上下文（画笔）
     let ctx = can_container.getContext('2d')
+    if (!ctx) {
+        console.error('抱歉，您的浏览器不支持canvas！请更新到最新浏览器')
+    }
+    // 填充色
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
+    // 形状
     ctx.fillRect(10, 10, 200, 200)
 })
