c97cfad420f4ef13174cd9bc98412f89d652b0e0
Seven Li-后会有七 <vinson7lhl@126.com>
2021-10-21 16:59:09 +0800

    feat/增加webGLS功能能测试

--- webGL/index.js
+++ webGL/index.js
@@ -1,4 +1,5 @@
 $(function() {
+    // 注意canvas只能用写在标签上的宽、高来定义大小；如果用脚本则会有兼容问题
     console.log('WebGL准备完毕')
     // 获取canvas-dom容器
     let can_container = $('#web_gl_container').get(0)
@@ -6,6 +7,7 @@ $(function() {
     let ctx = can_container.getContext('2d')
     if (!ctx) {
         console.error('抱歉，您的浏览器不支持canvas！请更新到最新浏览器')
+        return
     }
     // 填充色
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
