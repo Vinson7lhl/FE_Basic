17e5e588c4c17db765e16b785b9a2bf51099d006
Seven Li-后会有七 <vinson7lhl@126.com>
2021-10-21 18:00:20 +0800

    feat/webgl-demo

--- webGL/index.js
+++ webGL/index.js
@@ -4,13 +4,17 @@ $(function() {
     // 获取canvas-dom容器
     let can_container = $('#web_gl_container').get(0)
     // 获取绘制的上下文（画笔）
-    let ctx = can_container.getContext('2d')
-    if (!ctx) {
+    let gl = can_container.getContext('2d')
+    if (!gl) {
         console.error('抱歉，您的浏览器不支持canvas！请更新到最新浏览器')
         return
     }
     // 填充色
-    ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-    // 形状
-    ctx.fillRect(10, 10, 200, 200)
+    gl.fillStyle = 'rgba(0, 0, 255, 1)'
+    // 绘制矩形：前两个参数是举例画布左上角的位置值；后两者是矩形宽高
+    gl.fillRect(10, 10, 200, 200)
+    $('#clear_all').on('click', () => {
+        gl.clearColor(1, 0, 0, 1)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+    })
 })
